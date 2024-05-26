a538f3a0f78f4495358158e357176d53164e0fde
CMariaCortijo <cmariacortijo@gmail.com>
2024-05-26 17:16:05 +0200

    Delete Proyectophp directory

--- Proyectophp/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-//Conexión a la base de datos utilizando MySQLi /
-class Database{
-    public static function connect(){
-        $db = new mysqli('localhost', 'root', '', 'tienda_sakura');
-        $db->query("SET NAMES 'utf8'");
-        return $db;
-    }
-}
--- Proyectophp/config/parameters.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-//definición de las constantes
-//Define la ruta de la base del proyecto
-define("base_url", "http://localhost/Proyectophp/");
-//Define el controlador por defecto cuando no se especifique este
-define("controller_default", "productoController");
-//Si no se especifica nada por defecto será index
-define("action_default", "index");
